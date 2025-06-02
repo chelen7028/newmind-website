3beb1a9db5ac0f1b43bfa24e2037818a61a881d5
chelen <chelen@mit.edu>
2025-06-01 20:05:42 -0400

    debug

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'docs')));
 
 const CSV_FILE = path.join(__dirname, '..', 'data', 'applications.csv');
 
